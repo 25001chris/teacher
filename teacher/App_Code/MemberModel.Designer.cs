cb76c93588577b62ebc320740b397b561610916b
W10C\Admin <25001cchris@gmail.com>
2022-09-07 13:52:42 +0800

    test

--- teacher/App_Code/MemberModel.Designer.cs
+++ teacher/App_Code/MemberModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\練習\0907_TEST\20220907_練習1\teacher\teacher\App_Code\MemberModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\練習\0907_TEST\新增資料夾\teacher\App_Code\MemberModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
